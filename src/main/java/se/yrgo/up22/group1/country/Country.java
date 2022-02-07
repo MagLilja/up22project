2bcf1ae1abea343dde92feccf7653fe2b590e4e1
Magnus Lilja <magnus@connoisseur.se>
2022-02-07 18:59:31 +0100

    Added tests and more methods

--- src/main/java/se/yrgo/up22/group1/country/Country.java
+++ src/main/java/se/yrgo/up22/group1/country/Country.java
@@ -3,6 +3,7 @@ package se.yrgo.up22.group1.country;
 import se.yrgo.up22.group1.coach.Coach;
 import se.yrgo.up22.group1.player.Player;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 
@@ -19,11 +20,17 @@ public class Country {
 
     public Country(String name, List<Player> players, Coach coach) {
         this(name);
-        this.players = players;
         addPlayer(players);
         addCoach(coach);
     }
 
+
+    public Country(String name, int FIFARanking, double FIFARankingPoints) {
+        this(name);
+        addFIFARanking(FIFARanking);
+        addFIFARankingPoints(FIFARankingPoints);
+        this.FIFARankingPoints = FIFARankingPoints;
+    }
 
     public Country(String name, int FIFARanking, double FIFARankingPoints,
                    List<Player> players, Coach coach) {
@@ -35,16 +42,26 @@ public class Country {
 
 
     public void addPlayer(Player player) {
-
+        if (this.players == null) {
+            this.players = new ArrayList<>();
+        }
+        players.add(player);
     }
+
     // Overloading method.
-    private void addPlayer(List<Player> players) {
+    public void addPlayer(List<Player> players) {
+        if (this.players == null) {
+            this.players = players;
+        } else {
+            this.players.addAll(players);
+        }
     }
 
     private void addCoach(Coach coach) {
 
         this.coach = this.coach;
     }
+
     private void addFIFARankingPoints(double FIFARankingPoints) {
         if (validateFIFARankingPoints(FIFARankingPoints)) {
             this.FIFARankingPoints = this.FIFARankingPoints;
@@ -56,7 +73,7 @@ public class Country {
     // Bounds for points taken from FIFA official doc:
     // https://digitalhub.fifa.com/m/f99da4f73212220/original/edbm045h0udbwkqew35a-pdf.pdf
     private boolean validateFIFARankingPoints(double FIFARankingPoints) {
-        if (FIFARankingPoints > 1558 || FIFARankingPoints <= 804) {
+        if (FIFARankingPoints > 2109.09 || FIFARankingPoints <= 357) {
             return false;
         } else {
 
@@ -71,6 +88,7 @@ public class Country {
             throw new InputMismatchException("Not a valid FIFA Ranking");
         }
     }
+
     // 210 teams
     private boolean validateFIFARanking(int FIFARanking) {
         if (FIFARanking > 210 || FIFARanking <= 0) {
@@ -80,6 +98,30 @@ public class Country {
         }
     }
 
+    public String getName() {
+                return name;
+    }
 
+    public int getFIFARanking() {
+        return FIFARanking;
+    }
 
+    public double getFIFARankingPoints() {
+        return FIFARankingPoints;
+    }
+
+    public List<Player> getPlayers() {
+        if (players == null){
+            throw new NullPointerException("Country does not have any players yet");
+        }
+        return players;
+    }
+
+    public Coach getCoach() {
+        return coach;
+    }
+
+    public void removeAllPlayersFromTeam(){
+        players.clear();
+    }
 }
---
+++ src/test/java/se/yrgo/up22/group1/country/CountryTest.java
@@ -0,0 +1,146 @@
+package se.yrgo.up22.group1.country;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import se.yrgo.up22.group1.coach.Coach;
+import se.yrgo.up22.group1.player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryTest {
+    Country sweden;
+    Country usa;
+    Player caroline;
+    Player lina;
+    Player fridolina;
+    Coach peter;
+
+    @BeforeEach
+    void beforeEach() {
+        sweden = new Country("Sweden");
+        lina = new Player("Lina Hurtig");
+        caroline = new Player("Caroline Seger");
+        fridolina = new Player("Fridolina Rolfö");
+        peter = new Coach("Peter Gerhardsson");
+        usa = new Country("USA", 1, 2109.09);
+    }
+
+
+    @Test
+    @DisplayName("Can add one player test to empty country team")
+        // Will also assert that getName() and Country(String name) is passed.
+    void addOnePlayerToEmptyTeamTest() {
+        // given
+        Country sweden = new Country("Sweden");
+        // when
+        int expectedAmountOfPlayers = 1;
+        sweden.addPlayer(fridolina);
+        int amountOfPlayers = sweden.getPlayers().size();
+        // then
+        assertEquals(expectedAmountOfPlayers, amountOfPlayers);
+    }
+
+    @Test
+    @DisplayName("add list of players to empty team")
+    void addListOfPlayersWithAddPlayerMethodToEmptyTeamTest() {
+        // given
+        Player fridolina = new Player("Fridolina Rolfö");
+        Player lina = new Player("Lina Hurtig");
+        List<Player> squad = new ArrayList<>();
+        squad.add(fridolina);
+        squad.add(lina);
+        // when
+        sweden.addPlayer(squad);
+        int expectedAmountOfPlayers = 2;
+        int amountOfPlayers = sweden.getPlayers().size();
+        // then
+        assertEquals(expectedAmountOfPlayers, amountOfPlayers);
+
+    }
+
+    @Test
+    @DisplayName("add list of players to team with players")
+    void addListOfPlayersWithAddPlayerMethodToNotEmptyTeamTest() {
+        // given
+        Player fridolina = new Player("Fridolina Rolfö");
+        List<Player> squad = new ArrayList<>();
+        squad.add(fridolina);
+        List<Player> addToSquad = new ArrayList<>();
+        addToSquad.add(lina);
+        // when
+        sweden.addPlayer(squad);
+        sweden.addPlayer(addToSquad);
+        int expectedAmountOfPlayers = 2;
+        int amountOfPlayers = sweden.getPlayers().size();
+        // then
+        assertEquals(expectedAmountOfPlayers, amountOfPlayers);
+
+
+    }
+
+    @Test
+    @DisplayName("Can add a list of players with constructor test")
+    void addListOfPlayersWithConstructorTest() {
+        // given
+        List<Player> squad = new ArrayList<>();
+        squad.add(lina);
+        Country sweden1 = new Country("Sweden", squad, peter);
+        // when
+        int expectedAmountOfPlayers = 1;
+        int amountOfPlayers = sweden1.getPlayers().size();
+        // then
+        assertEquals(expectedAmountOfPlayers, amountOfPlayers);
+    }
+
+    @Test
+    void getNameTest() {
+        // when
+        String expectedName = "Sweden";
+        String actualName = sweden.getName();
+        // then
+        assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    void getFIFARankingTest() {
+        // given
+
+        // when
+        int expectedRanking = 1;
+        int actualRanking = usa.getFIFARanking();
+        // then
+        assertEquals(expectedRanking, actualRanking);
+    }
+
+    @Test
+    void getFIFARankingPoints() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getPlayers() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getCoach() {
+        // given
+
+        // when
+
+        // then
+    }
+}
