db4a4917ebc09d4d09920b47a1870c96e6ab5292
Eleni Solomon-Samuelsson <elesol2001@skola.goteborg.se>
2022-02-08 12:30:59 +0100

    Adding Match class

---
+++ src/main/java/se/yrgo/up22/group1/match/Match.java
@@ -0,0 +1,36 @@
+package se.yrgo.up22.group1.match;
+
+import java.time.ZonedDateTime;
+
+public class Match {
+    private String nationalTeamA;
+    private String nationalTeamB;
+    private String matchLocation;
+    private String arena;
+    private ZonedDateTime matchDateAndTime;
+    private int nationalTeamAScore;
+    private int nationalTeamBScore;
+    private int publicNumber;
+
+    public String getNationalTeamA() {
+        return nationalTeamA;
+    }
+
+    public String getNationalTeamB() {
+        return nationalTeamB;
+    }
+
+    public String getmatchLocation() {
+        return matchLocation;
+    }
+    
+    public String getarena() {
+        return arena;
+    }
+
+    public ZonedDateTime getmatchDateAndTime() {
+        return matchDateAndTime;
+    }
+
+}
+
