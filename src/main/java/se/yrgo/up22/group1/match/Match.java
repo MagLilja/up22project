286fdc98cc7eb99135f437ff299cffa489d2fad0
Eleni Solomon-Samuelsson <elesol2001@skola.goteborg.se>
2022-02-08 20:31:15 +0100

    Adding getters and more attributes

--- src/main/java/se/yrgo/up22/group1/match/Match.java
+++ src/main/java/se/yrgo/up22/group1/match/Match.java
@@ -1,18 +1,30 @@
 package se.yrgo.up22.group1.match;
 
 import java.time.ZonedDateTime;
+import java.util.List;
+import se.yrgo.up22.group1.Player.Player;
 
 public class Match {
     private String nationalTeamA;
     private String nationalTeamB;
-    private String matchLocation;
     private String arena;
     private ZonedDateTime matchDateAndTime;
     private int nationalTeamAScore;
     private int nationalTeamBScore;
     private int publicNumber;
+    private List <Player> nationalTeamAPlayers;
+    private List <Player> nationalTeamBPlayers;
 
-    public Match() {
+    public Match(String nationalTeamA, String nationalTeamB, String arena, ZonedDateTime matchDateAndTime, int nationalTeamAScore, int nationalTeamBScore, int publicNumber, List <Player> nationTeamAPlayers, List <Player> nationalTeamBPlayers){
+        this.nationalTeamA = nationalTeamA;
+        this.nationalTeamB = nationalTeamB;
+        this.arena = arena;
+        this.matchDateAndTime = matchDateAndTime;
+        this.nationalTeamAScore = nationalTeamAScore;
+        this.nationalTeamBScore = nationalTeamBScore;
+        this.publicNumber = publicNumber;
+        this.nationalTeamAPlayers = nationTeamAPlayers;
+        this.nationalTeamBPlayers = nationalTeamBPlayers; 
     }
 
     public String getNationalTeamA() {
@@ -21,10 +33,6 @@ public class Match {
 
     public String getNationalTeamB() {
         return nationalTeamB;
-    }
-
-    public String getmatchLocation() {
-        return matchLocation;
     }
     
     public String getarena() {
@@ -35,5 +43,24 @@ public class Match {
         return matchDateAndTime;
     }
 
+    public int getnationalTeamAScore() {
+        return nationalTeamAScore;
+    }
+
+    public int getnationalTeamBScore() {
+        return nationalTeamBScore;
+    }
+
+    public int getpublicNumber() {
+        return publicNumber;
+    }
+
+    public List <Player> nationalTeamAplayers() {
+        return nationalTeamAPlayers;
+    }
+
+    public List <Player> nationalTeamBplayers() {
+        return nationalTeamBPlayers;
+    }
 }
 
