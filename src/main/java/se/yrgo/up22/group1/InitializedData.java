8aa0407bd9e2af24f9116349abf9a4a6a4a48882
Magnus Lilja <magnus@connoisseur.se>
2022-02-10 15:43:05 +0100

    Added more data and a bit of Main methods

--- src/main/java/se/yrgo/up22/group1/InitializedData.java
+++ src/main/java/se/yrgo/up22/group1/InitializedData.java
@@ -1,6 +1,13 @@
 package se.yrgo.up22.group1;
 
+import se.yrgo.up22.group1.coach.Coach;
 import se.yrgo.up22.group1.country.Country;
+import se.yrgo.up22.group1.match.Match;
+import se.yrgo.up22.group1.player.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class InitializedData {
 
@@ -20,6 +27,9 @@ public class InitializedData {
     // Initialize 10 countries
     // Initialize a List<> of countries
 
+    List<Player> listOfPlayers = new ArrayList<>();
+    List<Match> listOfMatches = new ArrayList<>();
+    Coach coach = new Coach("peter");
     Country usa = new Country("USA", 1, 2109.09, listOfPlayers, listOfMatches, coach);
     Country sweden = new Country("Sweden", 2, 2079, listOfPlayers, listOfMatches, coach);
     Country germany = new Country("Germany", 3, 2070, listOfPlayers, listOfMatches, coach);
@@ -29,5 +39,15 @@ public class InitializedData {
     Country brazil = new Country("Brazil", 7, 1976, listOfPlayers, listOfMatches, coach);
     Country england = new Country("England", 8, 1974, listOfPlayers, listOfMatches, coach);
     Country spain = new Country("Spain", 9, 1959, listOfPlayers, listOfMatches, coach);
-    Country KoreaDPR = new Country("Korea DPR", 9, 1904, listOfPlayers, listOfMatches, coach);
+    Country koreaDPR = new Country("Korea DPR", 9, 1904, listOfPlayers, listOfMatches, coach);
+
+    List<Country> listOfCountries = new ArrayList<>();
+
+    public InitializedData() {
+        Collections.addAll(listOfCountries, sweden, usa, germany, france, netherlands, canada, brazil, england, spain, koreaDPR);
+    }
+
+    public List<Country> getListOfCountries() {
+        return listOfCountries;
+    }
 }
--- src/main/java/se/yrgo/up22/group1/Main.java
+++ src/main/java/se/yrgo/up22/group1/Main.java
@@ -4,8 +4,23 @@ package se.yrgo.up22.group1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("HEj HEJ!");
+        InitializedData initializedData = new InitializedData();
 
+
+        /*
+        Damfotboll!
+        Välj nedan:
+        1.Se alla Landslag (sorterade efter FIFA-ranking)
+        2.Se alla Spelare (sorterade efter...)
+        3.Se alla Matcher (sorterade efter...)
+        4.Se alla Coacher (sortera efter...)
+
+
+         */
+
+        initializedData.getListOfCountries().stream()
+//                .sorted()
+                .forEach(System.out::println);
 
     }
 }
