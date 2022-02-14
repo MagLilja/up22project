5a5de3d255c0edd86ccf6e2368994c7b86d68b71
rayansabah <raymah0712@skola.goteborg.se>
2022-02-14 20:26:52 +0100

    Added some comments to the PlayerTest class

--- src/test/java/se/yrgo/up22/group1/player/PlayerTest.java
+++ src/test/java/se/yrgo/up22/group1/player/PlayerTest.java
@@ -21,6 +21,7 @@ public class PlayerTest {
 
 
     ////PLayer One
+    /// This code is testing so you can put in Name, Age , Club, Games Played and Goal Scored
     @Test
     public void testAddPlayer() {
         String expName = "Lina Hurtig";
@@ -28,25 +29,25 @@ public class PlayerTest {
         testPlayerOne.getName();
         String result = testPlayerOne.getName();
         assertEquals(expName, result);
-        /// Age
+        /// Testing input of Age
         int expAge = 26;
         testPlayerOne.setAge(26);
         testPlayerOne.getAge();
         int ageResult = testPlayerOne.getAge();
         assertEquals(expAge, ageResult);
-        /// Club
+        /// Testing put in witch Club the player is playing in
         String expClub = "Juventus";
         testPlayerOne.setClub("Juventus");
         testPlayerOne.getClub();
         String clubResult = testPlayerOne.getClub();
         assertEquals(expClub, clubResult);
-        /// Games Played
-        int expGamesPlayed = 164;
+        /// Testing input of how many played matches to player has played
+        int expPlayedMatches = 164;
         testPlayerOne.setPlayedMatches(164);
         testPlayerOne.getPlayedMatches();
         int playedMatchesResult = testPlayerOne.getPlayedMatches();
-        assertEquals(expGamesPlayed, playedMatchesResult);
-        /// Goals Scored
+        assertEquals(expPlayedMatches, playedMatchesResult);
+        /// Testing input of how many Goals Scored the player has
         int expGoalsScored = 49;
         testPlayerOne.setGoalsScored(49);
         testPlayerOne.getGoalsScored();
@@ -57,6 +58,7 @@ public class PlayerTest {
 
 
     /// Player Two
+    /// This code is testing so you can put in Name, Age , Club, Games Played and Goal Scored
     @Test
     public void testAddPlayerTwo() {
         String expName = "Caroline Seger";
@@ -64,25 +66,25 @@ public class PlayerTest {
         testPlayerTwo.getName();
         String result = testPlayerTwo.getName();
         assertEquals(expName, result);
-        /// Age
+        /// Testing input of Age
         int expAge = 26;
         testPlayerTwo.setAge(26);
         testPlayerTwo.getAge();
         int ageResult = testPlayerTwo.getAge();
         assertEquals(expAge, ageResult);
-        /// Club
+        /// Testing put in witch Club the player is playing in
         String expClub = "Juventus";
         testPlayerTwo.setClub("Juventus");
         testPlayerTwo.getClub();
         String clubResult = testPlayerTwo.getClub();
         assertEquals(expClub, clubResult);
-        ///Games Played
-        int expGamesPlayed = 211;
+        /// Testing input of how many played matches to player has played
+        int expPlayedMatches = 211;
         testPlayerTwo.setPlayedMatches(211);
         testPlayerTwo.getPlayedMatches();
         int gamesPlayedResult = testPlayerTwo.getPlayedMatches();
-        assertEquals(expGamesPlayed, gamesPlayedResult);
-        /// Goals Scored
+        assertEquals(expPlayedMatches, gamesPlayedResult);
+        /// Testing input of how many Goals Scored the player has
         int expGoalsScored = 26;
         testPlayerTwo.setGoalsScored(26);
         testPlayerTwo.getGoalsScored();
